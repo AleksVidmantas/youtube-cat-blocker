88200246e4c2ec906d1d4fb78b3386fd2c9e9075
Aleks <bobopirate1010@gmail.com>
2018-09-18 21:55:53 -0700

    Succesful test of plugin, blocks music videos :/

--- plugin/cat-blocker.js
+++ plugin/cat-blocker.js
@@ -1,37 +1,66 @@
+var ourRequest = new XMLHttpRequest();
+var catMap = new Map();
+var urlRX = /(v=)(...........)/ //this is the regex that finds the v=etc code
 
+//necessary
+catMap.set("1", "Film & Animation");
+catMap.set("10", "Music");
+catMap.set("15", "Pets & Animals");
+catMap.set("17", "Sports")
+catMap.set("18", "Short Movies");
+catMap.set("19", "Travel & Events");
+catMap.set("20", "Gaming");
+catMap.set("21", "Videoblogging");
+catMap.set("22", "People and Blogs");
+catMap.set("23", "Comdey");
+catMap.set("24", "Entertainment");
+catMap.set("25", "News & Politics");
+catMap.set("26", "Howto & Style");
+catMap.set("27", "Education");
+catMap.set("28", "Science & Technology");
+catMap.set("29", "Nonprofits & Activism");
+catMap.set("30", "Movies");
+catMap.set("31", "Anime/Animation");
+catMap.set("32", "Action/Adventure");
+catMap.set("33", "Classics");
+catMap.set("34", "Comedy");
+catMap.set("35", "Documentary");
+catMap.set("36", "Drama");
+catMap.set("37", "Family");
+catMap.set("38", "Foreign");
+catMap.set("39", "Horror");
+catMap.set("40", "Sci-Fi/Fantasy");
+catMap.set("41", "Thriller");
+catMap.set("42", "Shorts");
+catMap.set("43", "Shows");
+catMap.set("44", "Trailers");
 
-console.log("test000000000");
+function getID(){
+	console.log("Printing...");
+    //var url = document.getElementById("youtubeURL").value;
+    var url = window.location.href;
+    console.log("Passing " + url);
+    var urlKey = urlRX.exec(url)[2];//group that matches the urlKey for ourRequests.open()
+    console.log("KEY: " + urlKey);
 
-var xhr = new XMLHttpRequest();
-console.log('UNSENT', xhr.status);
+    ourRequest.open('GET', 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id=' + urlKey + '&key=AIzaSyC59K1CO0RwZ-WsANfM4H8bqIXRtFdPcEI');
 
-
-
-console.log('OPENED', xhr.status);
-if ("withCredentials" in xhr) {
-	console.log("CORS supported");
-	xhr.open('GET', 'https://xhr.spec.whatwg.org/', true);
-	console.log("openbed>");
-}else{
-	console.log("Sh*t");
-}
-
-// Response handlers.
-xhr.onload = function() {
-	console.log("testonload");
-    var text = xhr.responseText;
+    ourRequest.onload = function() {
+        var ourData = JSON.parse(ourRequest.responseText);
+        var catID = ourData['items'][0]['snippet']['categoryId'];
+        cat = catMap.get(catID); //uses map to get category
+        console.log(cat);
+        //document.getElementById("category").textContent = "Category: " + cat;
+		if(cat === "Music"){
+			console.log("Cat is musc");
+			window.location.href = "http://lmgtfy.com/?q=Study+you+dumbass";
+		}else{
+			console.log("Cat was infact + " + cat);
+		}
     
-    alert('Response from CORS request to ' + text);
-  };
-
-xhr.onerror = function() {
-alert('Woops, there was an error making the request.: ' + xhr.statusText);
-};
-
-xhr.send();
-
-
-
-
-
-
+    };
+    ourRequest.send();
+    
+}
+console.log("Pre getID()");
+getID();
